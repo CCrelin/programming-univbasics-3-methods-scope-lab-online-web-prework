4ea3a921f33f506242cad301840e7b87a1687154
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-09 01:32:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,6 +5,12 @@ end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  warning = "It's Dangerous To Go Alone! Take This."
-  puts status, warning
+  puts status
 end
+
+def link
+  warning = "It's Dangerous To Go Alone! Take This."
+  puts warning
+end
+
+  
